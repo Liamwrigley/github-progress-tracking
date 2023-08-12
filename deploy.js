d09c7c8fc9177283c27a6db6ec2b62ad252d5968
Liamwrigley <liamwrigley01@gmail.com>
2023-08-12 13:54:28 +1000

    resolve secret

--- deploy.js
+++ deploy.js
@@ -1,28 +1,43 @@
 const { exec } = require('child_process');
 const express = require("express");
+const crypto = require('crypto');
 const router = express.Router();
 const webhook_helper = require('./functions/discord')
 
-router.post("/deploy", async (req, res) => {
-    const secret = process.env.DEPLOY_SECRET
+const verifyGitHubPayload = (req, res, next) => {
+    const payload = JSON.stringify(req.body);
+    if (!payload) {
+        return res.status(400).send('Request body empty');
+    }
+
+    const sig = req.get('X-Hub-Signature-256');
+    if (!sig) {
+        return res.status(401).send('Missing X-Hub-Signature-256');
+    }
+
+    const hmac = crypto.createHmac('sha256', process.env.DEPLOY_SECRET);
+    const digest = 'sha256=' + hmac.update(payload).digest('hex');
+
+    if (sig !== digest) {
+        return res.status(401).send('Mismatched X-Hub-Signature-256');
+    }
+
+    return next();
+}
+
+router.post("/deploy", verifyGitHubPayload, async (req, res) => {
     await webhook_helper.sendInfoReport("incomming deploy event")
 
-    var event = req.body
-    if (secret == event.hook.config.secret) {
-        exec('', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-        });
+    exec('', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
 
-        res.sendStatus(200);
-    } else {
-        console.error("secret did not match for deploy")
-        res.status(401).send("Secret did not match")
-    }
+    res.sendStatus(200);
 })
 
 module.exports = router;
