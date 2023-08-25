573ee0cd135b0a325fa3ec83771124c60cda4a6a
Liamwrigley <liamwrigley01@gmail.com>
2023-08-25 15:41:36 +1000

    upd 5

--- ui/src/components/layout/home.jsx
+++ ui/src/components/layout/home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
                 <h2>TODO</h2>
             </div>
             <div className="border-2 border-custom-highlight2 rounded p-2">
-                <h2>Auth process - updated deploy@4</h2>
+                <h2>Auth process - updated deploy@5</h2>
                 <ul className="steps steps-vertical">
                     <li className="step">landing auth page (asks to connect with either github or discord)</li>
                     <li className="step">user page - shows details for whatever is linked</li>
