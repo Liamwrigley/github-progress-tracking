eeaea5d24a697ecbe1cda7480d31d372b5c76067
Liamwrigley <liamwrigley01@gmail.com>
2023-08-25 13:04:49 +1000

    ui update

--- ui/src/components/layout/home.jsx
+++ ui/src/components/layout/home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
                 <h2>TODO</h2>
             </div>
             <div className="border-2 border-custom-highlight2 rounded p-2">
-                <h2>Auth process</h2>
+                <h2>Auth process - does this update?</h2>
                 <ul className="steps steps-vertical">
                     <li className="step">landing auth page (asks to connect with either github or discord)</li>
                     <li className="step">user page - shows details for whatever is linked</li>
