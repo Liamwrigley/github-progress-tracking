d8388fbb2ca5b92c868bb6477266858c2a95a1e1
Liamwrigley <liamwrigley01@gmail.com>
2023-08-25 19:45:05 +1000

    revert debugging changes

--- ui/src/pages/auth.jsx
+++ ui/src/pages/auth.jsx
@@ -19,26 +19,25 @@ export const Auth = () => {
     const GITHUB_REDIRECT = () => `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo_hook%20repo%20user`
 
 
-    // if (!isLoading) {
-    //     if (!userData.authenticated) {
-    //         window.location.href = DISCORD_REDIRECT()
-    //     }
-    //     // github not setup at all
-    //     if (userData.authenticated && userData.user && Object.keys(userData.user.github).length === 0) {
-    //         window.location.href = GITHUB_REDIRECT()
-    //     }
-    //     if (userData.authenticated && userData.user && !userData.user.github.repo && userData.needsGithubToken) {
-    //         window.location.href = GITHUB_REDIRECT()
-    //     }
-    //     if (userData.authenticated && userData.user && !userData.user.github.repo) {
-    //         return <SelectRepo user={userData.user} refetch={refetch} />
-    //     }
-    // }
+    if (!isLoading) {
+        if (!userData.authenticated) {
+            window.location.href = DISCORD_REDIRECT()
+        }
+        // github not setup at all
+        if (userData.authenticated && userData.user && Object.keys(userData.user.github).length === 0) {
+            window.location.href = GITHUB_REDIRECT()
+        }
+        if (userData.authenticated && userData.user && !userData.user.github.repo && userData.needsGithubToken) {
+            window.location.href = GITHUB_REDIRECT()
+        }
+        if (userData.authenticated && userData.user && !userData.user.github.repo) {
+            return <SelectRepo user={userData.user} refetch={refetch} />
+        }
+    }
 
     return (
         <div>
             <div className='flex flex-col gap-4'>
-                <a href={DISCORD_REDIRECT()}>DISCORD TEST</a>
                 {userData?.user && <>
                     <div className="flex items-center space-x-3">
                         <div className="group-hover/list:animate-blink opacity-0 -translate-x-1 group-hover/list:opacity-100 group-hover/list:translate-x-0 duration-100">
