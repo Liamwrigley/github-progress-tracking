afbf9f161f6b6d8105f297705ccad5794a8e3fb6
Liamwrigley <liamwrigley01@gmail.com>
2023-08-25 11:34:42 +1000

    fix deploy script

--- api/deploy.sh
+++ api/deploy.sh
@@ -6,8 +6,19 @@ cd /root/github-progress-tracking
 # Pull latest changes
 git pull origin main 
 
-# Install any new dependencies
+# Install dependencies for the root (if any)
 npm install
+
+# Navigate to api directory and install its dependencies
+cd api
+npm install
+cd ..
+
+# Navigate to ui directory and install its dependencies
+cd ui
+npm install
+npm run build
+cd ..
 
 # Restart PM2 process
 pm2 restart github-project-tracker
